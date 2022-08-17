006be90b28a4dacfee1e1c3e78387d5f121f0e51
DebjitPramanick <dpramanick9990@gmail.com>
2022-08-17 21:00:40 +0530

    Feat: Setup flowbite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Routes>
+          <Route path='/'/>
+        </Routes>
+      </Router>
     </div>
   );
 }
