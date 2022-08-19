094d348a573a76d31f9f24fd9a8daf833a4941d0
DebjitPramanick <dpramanick9990@gmail.com>
2022-08-19 21:04:16 +0530

    Feat: Register UI

--- client/src/pages/Register/RegitserUI.tsx
+++ client/src/pages/Register/RegitserUI.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../../components/FormFields/Button'
 import InputField from '../../components/FormFields/InputField'
 import AuthLayout from '../../layouts/AuthLayout'
 import FormLayout from '../../layouts/FormLayout'
@@ -47,22 +49,9 @@ const RegitserUI = () => {
                         required={true}
                     />
 
+                    <Button label="Register" type="submit" rightAligned={true}/>
 
-                    <div className="flex items-start mb-6">
-                        <div className="flex items-center h-5">
-                            <input
-                                id="remember"
-                                type="checkbox" value=""
-                                className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
-                                required />
-                        </div>
-                        <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                    </div>
-                    
-                    <button type="submit"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                        Register
-                    </button>
+                    <Link to="/login" className='block mt-4 text-sm'>Already an user? Login</Link>
                 </form>
             </FormLayout>
         </AuthLayout>
