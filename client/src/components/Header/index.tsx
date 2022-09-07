ddf2bf579755170b9379b11fef2e933b23f07820
DebjitPramanick <dpramanick9990@gmail.com>
2022-09-07 21:43:34 +0530

    Fix: Max width header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='fixed w-full z-20 top-0 left-0 shadow-lg' style={{ background: '#c5fbd3' }}>
             <nav className="px-2 py-2.5 max-w-6xl mx-auto z-20 top-0 left-0 border-b border-gray-200">
-                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                <div className="flex flex-wrap justify-between items-center mx-auto">
                 <Link to="/" className="flex items-center">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                         <span className="self-center text-xl font-semibold whitespace-nowrap">Story Teller</span>
