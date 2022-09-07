1a713c95d42ded3e3f1c13ef3adcee2898e1c43c
DebjitPramanick <dpramanick9990@gmail.com>
2022-09-07 22:00:38 +0530

    Fix: Feed ui modification

--- client/src/components/Feed/FeedUI.tsx
+++ client/src/components/Feed/FeedUI.tsx
@@ -59,7 +59,7 @@ const FeedUI: React.FC<FeedUIProps> = ({
 
                     <div onClick={handleSaveFeed}>
                         {!isSaved ? <BookmarkBorderIcon />
-                        : <BookmarkIcon style={{ color: "#00d0cd" }} />}
+                            : <BookmarkIcon style={{ color: "#00d0cd" }} />}
                     </div>
                 </div>
             </div>
@@ -69,19 +69,27 @@ const FeedUI: React.FC<FeedUIProps> = ({
                 ))}
             </div>
 
-            <a href="/">
+            <a href="/" className='block mx-4 my-2 shadow-md rounded-lg overflow-hidden'>
                 {feed.cover !== '' ? (
-                    <div className="rounded-t-lg h-44 flex items-center justify-center">
+                    <div className="h-44 flex items-center justify-center">
                         <img src={feed.cover} alt="/" className='h-full' />
                     </div>
                 ) : (
-                    <div className="rounded-t-lg h-44 flex items-center justify-center">
+                    <div className="h-44 flex items-center justify-center">
                         <ImageNotSupportedIcon style={{ fontSize: '80px', color: 'grey' }} />
                     </div>
                 )}
             </a>
 
-            <p className='px-4 py-2'>{likeCounts} Liked | {savedCounts} Saved</p>
+            <div className='flex items-center px-4 py-2 gap-4'>
+                <p className='py-2 text-sm text-gray-400'>
+                    <FavoriteIcon style={{ color: "red", fontSize: '18px', marginTop: '-4px' }} /> {likeCounts}
+                </p>
+
+                <p className='py-2 text-sm text-gray-400'>
+                    <BookmarkIcon style={{ color: "#00d0cd", fontSize: '18px', marginTop: '-4px' }} /> {savedCounts}
+                </p>
+            </div>
 
             <div className="p-5">
                 <a href="/">
@@ -89,7 +97,7 @@ const FeedUI: React.FC<FeedUIProps> = ({
                 </a>
                 <div className="mb-3" ref={contentRef}>
                 </div>
-                <a href="/" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="/" className="inline-flex items-center py-3 px-3 text-sm font-medium text-center text-black bg-blue-200 rounded hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300">
                     Read more
                     <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
