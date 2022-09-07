13e535df940109dd4ee86352aeaec3b683709b2e
DebjitPramanick <dpramanick9990@gmail.com>
2022-09-07 22:34:34 +0530

    Fix: Pagelayout height

--- client/src/layouts/PageLayout/index.tsx
+++ client/src/layouts/PageLayout/index.tsx
@@ -8,7 +8,7 @@ const PageLayout: React.FC<any> = ({ children }) => {
 
     return (
         <div className={`max-w-6xl mx-auto flex px-2 py-4 ml-auto relative ${!isAuthPage ? 'gap-6' : ''}`}
-            style={{ marginTop: '61px', background: '#fbfbfb' }}>
+            style={{ marginTop: '61px', background: '#fbfbfb', minHeight: 'calc(100vh - 64px)' }}>
             <div style={{ width: isAuthPage ? '0px' : '288px' }}></div>
             <Sidebar />
             <div className='px-4 py-4 bg-white border rounded-lg' style={{ width: isAuthPage ? '100%' : 'calc(100% - 256px)' }}>
