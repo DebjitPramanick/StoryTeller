72c38405faba50488a377d31c52183543e7be30d
DebjitPramanick <dpramanick9990@gmail.com>
2023-02-09 14:05:45 +0530

    Fix: Get env using OS

--- server/database/connection.go
+++ server/database/connection.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"server/helpers"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,8 +18,11 @@ var SavedFeeds *mongo.Collection
 var FeedLikes *mongo.Collection
 
 func ConnectDB() {
-	connectionURL := helpers.GetEnvVariable("MONGO_URI")
-	dbName := helpers.GetEnvVariable("DB_NAME")
+	// connectionURL := helpers.GetEnvVariable("MONGO_URI")
+	// dbName := helpers.GetEnvVariable("DB_NAME")
+
+	connectionURL := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("DB_NAME")
 
 	clientOption := options.Client().ApplyURI(connectionURL)
 	client, err := mongo.Connect(context.TODO(), clientOption)
